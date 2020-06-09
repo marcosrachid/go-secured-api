55016326eb97597c006fb8e6e102138d3ad2cdae
Marcos Rachid <marcosrachid@gmail.com>
2020-06-09 15:05:03 -0300

    only one connection

--- internal/api/routes.go
+++ internal/api/routes.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 
 	"github.com/marcosrachid/go-secured-api/internal/db"
+	"github.com/marcosrachid/go-secured-api/internal/models/dto"
 	"github.com/marcosrachid/go-secured-api/internal/models/repository"
 
 	"github.com/gorilla/mux"
@@ -27,7 +28,7 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	cur, err := collection.Find(context.TODO(), bson.M{})
 
 	if err != nil {
-		db.GetError(err, w)
+		getError(err, w, http.StatusInternalServerError)
 		return
 	}
 
@@ -51,7 +52,7 @@ func GetBooks(w http.ResponseWriter, r *http.Request) {
 	}
 
 	if err := cur.Err(); err != nil {
-		log.Fatal(err)
+		getError(err, w, http.StatusInternalServerError)
 	}
 
 	json.NewEncoder(w).Encode(books) // encode similar to serialize process.
@@ -75,7 +76,7 @@ func GetBook(w http.ResponseWriter, r *http.Request) {
 	err := collection.FindOne(context.TODO(), filter).Decode(&book)
 
 	if err != nil {
-		db.GetError(err, w)
+		getError(err, w, http.StatusNotFound)
 		return
 	}
 
@@ -97,7 +98,7 @@ func CreateBook(w http.ResponseWriter, r *http.Request) {
 	result, err := collection.InsertOne(context.TODO(), book)
 
 	if err != nil {
-		db.GetError(err, w)
+		getError(err, w, http.StatusInternalServerError)
 		return
 	}
 
@@ -137,7 +138,7 @@ func UpdateBook(w http.ResponseWriter, r *http.Request) {
 	err := collection.FindOneAndUpdate(context.TODO(), filter, update).Decode(&book)
 
 	if err != nil {
-		db.GetError(err, w)
+		getError(err, w, http.StatusNotFound)
 		return
 	}
 
@@ -164,9 +165,22 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	deleteResult, err := collection.DeleteOne(context.TODO(), filter)
 
 	if err != nil {
-		db.GetError(err, w)
+		getError(err, w, http.StatusNotFound)
 		return
 	}
 
 	json.NewEncoder(w).Encode(deleteResult)
+}
+
+func getError(err error, w http.ResponseWriter, status int) {
+	log.Println(err.Error())
+	var response = dto.ErrorResponse{
+		ErrorMessage: err.Error(),
+		StatusCode:   status,
+	}
+
+	message, _ := json.Marshal(response)
+
+	w.WriteHeader(response.StatusCode)
+	w.Write(message)
 }
--- internal/db/db.go
+++ internal/db/db.go
@@ -2,12 +2,9 @@ package db
 
 import (
 	"context"
-	"encoding/json"
 	"fmt"
 	"log"
-	"net/http"
 
-	"github.com/marcosrachid/go-secured-api/internal/models/dto"
 	"github.com/marcosrachid/go-secured-api/pkg/utils"
 
 	"go.mongodb.org/mongo-driver/mongo"
@@ -19,35 +16,25 @@ const (
 	DATABASE  = "go-template"
 )
 
+var collection *mongo.Collection
+
 func ConnectDB() *mongo.Collection {
 
-	// Set client options
-	clientOptions := options.Client().ApplyURI(utils.GetenvDefault("MONGO_URI", MONGO_URI))
+	if collection == nil {
+		// Set client options
+		clientOptions := options.Client().ApplyURI(utils.GetenvDefault("MONGO_URI", MONGO_URI))
 
-	// Connect to MongoDB
-	client, err := mongo.Connect(context.TODO(), clientOptions)
+		// Connect to MongoDB
+		client, err := mongo.Connect(context.TODO(), clientOptions)
 
-	if err != nil {
-		log.Fatal(err)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println("Connected to MongoDB!")
+
+		collection = client.Database(utils.GetenvDefault("DATABASE", DATABASE)).Collection("books")
 	}
-
-	fmt.Println("Connected to MongoDB!")
-
-	collection := client.Database(utils.GetenvDefault("DATABASE", DATABASE)).Collection("books")
 
 	return collection
-}
-
-func GetError(err error, w http.ResponseWriter) {
-
-	log.Fatal(err.Error())
-	var response = dto.ErrorResponse{
-		ErrorMessage: err.Error(),
-		StatusCode:   http.StatusInternalServerError,
-	}
-
-	message, _ := json.Marshal(response)
-
-	w.WriteHeader(response.StatusCode)
-	w.Write(message)
 }
